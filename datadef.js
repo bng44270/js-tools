a4efb2b64ff2b9fc59bc95f563e7a0b1de80bc3b
Andy Carlson <bng44270@gmail.com>
2025-07-07 13:18:44 -0500

    Update datadef.js
    added deleteAll method

--- datadef.js
+++ datadef.js
@@ -1,32 +1,32 @@
 /*
 
   DataDef - Create a schema-enforced data structure based on the Array object
-  
+
   Usage:
-  
-    // Available data types:  
+
+    // Available data types:
     //	  DataDef.StringType
     //	  DataDef.NumberType
     //	  DataDef.BooleanType
-	
+
     var people = new DataDef({
       "name" : DataDef.StringType,
       "age" : DataDef.NumberType,
       "town" : DataDef.StringType
     });
-    
+
     people.insert({
       name: "Bob",
       age: 30,
       town: "New York"
     });
-    
+
     people.insert({
       name: "Jim",
       age: 33,
       town: "Chicago"
     });
-    
+
     people.insert({
       name:"Pete",
       age:50,
@@ -81,51 +81,51 @@ class DataDef extends Array{
 
 	constructor(fields,data) {
 		super();
-		
+
 		this.setSchema(fields);
-		
+
 		if (data) this.bulkInsert(data);
 	}
-	
+
 	setSchema(fields) {
 		var fieldNames = Object.keys(fields);
-		
+
 		this.SCHEMA = {};
-		
+
 		fieldNames.forEach(f => {
 			this.addField(f,fields[f]);
 		});
 	}
-	
+
 	addField(field,type) {
 		var fieldExists = Object.keys(this.SCHEMA).indexOf(field) > -1;
-		
+
 		if (fieldExists) {
 			throw new SchemaError("Field already exists (" + field + ")");
 		}
 		else {
 			var fieldTypes = [DataDef.NumberType,DataDef.StringType,DataDef.BooleanType];
-			
+
 			if (fieldTypes.indexOf(type) == -1) {
 				throw new SchemaError("Unknown data type (" + fields[fieldNames[i]]  + ")");
 			}
-			
+
 			this.SCHEMA[field] = type;
-			
+
 			this.forEach(r => {
 				r[field] = null;
 			});
 		}
 	}
-	
+
 	deleteField(field) {
 		var fieldExists = this.SCHEMA.indexOf(field) > -1;
-		
+
 		if (fieldExists) {
 			this.forEach(r => {
 				delete r[field];
 			});
-			
+
 			var fIdx = this.SCHEMA.indexOf(field);
 			delete this.SCHEMA[fIdx];
 		}
@@ -133,7 +133,7 @@ class DataDef extends Array{
 			throw new SchemaError("Field does not exist (" + field + ")");
 		}
 	}
-	
+
 	bulkInsert(records) {
 		records.forEach(r => {
 			this.insert(r);
@@ -157,7 +157,7 @@ class DataDef extends Array{
 						break;
 					}
 				}
-				
+
 			}
 
 			return match;
@@ -183,7 +183,7 @@ class DataDef extends Array{
 						break;
 					}
 				}
-				
+
 			}
 
 			return match;
@@ -197,25 +197,29 @@ class DataDef extends Array{
 
 	insert(record) {
 		var fields = Object.keys(record);
-		
+
 		//Fill empty fields
 		Object.keys(this.SCHEMA).filter(f => (fields.indexOf(f) == -1) ? true : false).forEach(f => {
 			record[f] = null;
 		});
-		
+
 		for (var i = 0; i < fields.length; i++) {
 			if (Object.keys(this.SCHEMA).indexOf(fields[i]) == -1) {
 				throw new SchemaError("Field not present in schema (" + fields[i] +")");
 			}
-			
+
 			if (typeof(record[fields[i]]) != this.SCHEMA[fields[i]]) {
 				throw new SchemaError("Field type mismatch (" + fields[i] + " expected " + this.SCHEMA[fields[i]] + " but got " + typeof(record[fields[i]]) + ")");
 			}
 		}
-		
+
 		this.push(record);
 	}
-	
+
+	deleteAll() {
+		while (this.pop()) continue;
+	}
+
 	delete(queryArray) {
 		this.map((r,rIdx) => {
 			return {i : rIdx, record : r}
